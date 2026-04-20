4017fb451771f35253d712d48cc57b066bd834d2
Ljy9515 <1427410613@qq.com>
2026-04-20 19:30:55 +0800

    11
    美术资源上传
    
    ^ Conflicts:
    ^	Assets/Test_art_boss01.png
    ^	Assets/Test_art_boss01.png.meta
    ^	Assets/background/Test_art_bg.png
    ^	Assets/background/Test_art_bg.png.meta

--- Assets/src/Script/UI/Config/UIPathRegistry.Generated.cs
+++ Assets/src/Script/UI/Config/UIPathRegistry.Generated.cs
@@ -12,10 +12,33 @@ public static partial class UIPathRegistry
         RegisterPrefab("battleRoot", "UIPrefab/CommonPrefabs/battleRoot");
         RegisterPrefab("CommonBackBoxItem", "UIPrefab/CommonPrefabs/CommonBackBoxItem");
         RegisterPrefab("UICanvas", "UIPrefab/CommonPrefabs/UICanvas");
+        RegisterImage("Test_art_boss02", "Image/icon/battle/Test_art_boss02");
+        RegisterImage("Test_art_guai01", "Image/icon/battle/Test_art_guai01");
+        RegisterImage("Test_art_guai02", "Image/icon/battle/Test_art_guai02");
+        RegisterImage("Test_art_jeuse01", "Image/icon/battle/Test_art_jeuse01");
+        RegisterImage("Test_art_jeuse02", "Image/icon/battle/Test_art_jeuse02");
+        RegisterImage("Test_art_jeuse03", "Image/icon/battle/Test_art_jeuse03");
         RegisterImage("test_battle_bg", "Image/icon/battle/test_battle_bg");
+        RegisterImage("Test_ui_baoxiang01", "Image/icon/battle/Test_ui_baoxiang01");
+        RegisterImage("Test_ui_baoxiang02", "Image/icon/battle/Test_ui_baoxiang02");
+        RegisterImage("Test_ui_kuangshi01", "Image/icon/battle/Test_ui_kuangshi01");
+        RegisterImage("Test_ui_wuqi_dun01", "Image/icon/battle/Test_ui_wuqi_dun01");
+        RegisterImage("Test_ui_wuqi_fa01", "Image/icon/battle/Test_ui_wuqi_fa01");
+        RegisterImage("Test_ui_wuqi_gon01", "Image/icon/battle/Test_ui_wuqi_gon01");
+        RegisterImage("Test_ui_wuqi_jian01", "Image/icon/battle/Test_ui_wuqi_jian01");
+        RegisterImage("Test_ui_wuqi_shipin01", "Image/icon/battle/Test_ui_wuqi_shipin01");
+        RegisterImage("Test_ui_card01", "Image/icon/card/Test_ui_card01");
+        RegisterImage("Test_ui_card02", "Image/icon/card/Test_ui_card02");
+        RegisterImage("Test_ui_card03", "Image/icon/card/Test_ui_card03");
+        RegisterImage("Test_ui_card04", "Image/icon/card/Test_ui_card04");
+        RegisterImage("Test_ui_carddi01 1", "Image/icon/card/Test_ui_carddi01 1");
         RegisterImage("Test_ui_carddi01", "Image/icon/card/Test_ui_carddi01");
         RegisterImage("Test_ui_biaotidi01", "Image/icon/kuang/Test_ui_biaotidi01");
         RegisterImage("Test_ui_biaotidi02", "Image/icon/kuang/Test_ui_biaotidi02");
+        RegisterImage("Test_ui_btn01", "Image/icon/kuang/Test_ui_btn01");
         RegisterImage("Test_ui_kuang01", "Image/icon/kuang/Test_ui_kuang01");
+        RegisterImage("ui_bagbiaoqiandi02", "Image/icon/kuang/ui_bagbiaoqiandi02");
+        RegisterImage("ui_btndi01", "Image/icon/kuang/ui_btndi01");
+        RegisterImage("ui_btndi02", "Image/icon/kuang/ui_btndi02");
     }
 }
